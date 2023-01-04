a1fe2eee96559fab44ce283642789210f77ca3a5
AlexisRodenas <alexis.rodenas@free.fr>
2023-01-04 18:04:20 +0100

    add sujet and form for create sujet

---
+++ src/Controller/SujetController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sujet;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SujetController extends AbstractController
+{
+    #[Route('/sujets/add', name: 'app_sujet_add')]
+    #[Route('/sujets/{id}/edit', name: 'app_sujet_edit')]
+    public function addEditSujet(Sujet $sujet = null, Request $request, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        if(!$sujet) {
+            $sujet = new Sujet();
+        }
+
+        $form = $this->createFormBuilder($sujet)
+            ->add('titre', TextType::class)
+            ->add('Valider', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($sujet);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('sujets');
+        }
+
+        return $this->render('sujet/add_edit.html.twig', [
+            'sujetForm' => $form->createView()
+        ]);
+    }
+
+    #[Route('/sujets', name: 'sujets')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Sujet::class);
+        $sujets = $repository->findAll();
+
+        return $this->render('sujet/index.html.twig', [
+            'controller_name' => 'SujetController',
+            'truc2' => $sujets
+        ]);
+    }
+}
---
+++ src/Entity/Sujet.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SujetRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SujetRepository::class)]
+class Sujet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $Titre = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $DateCreation = null;
+
+    #[ORM\OneToMany(mappedBy: 'sujet', targetEntity: Message::class, orphanRemoval: true)]
+    private Collection $messages;
+
+    public function __construct()
+    {
+        $this->messages = new ArrayCollection();
+        $this->DateCreation = new \DateTime();
+        //$this->verouillage = false;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->DateCreation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $DateCreation): self
+    {
+        $this->DateCreation = $DateCreation;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Message>
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Message $message): self
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages->add($message);
+            $message->setSujet($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Message $message): self
+    {
+        if ($this->messages->removeElement($message)) {
+            // set the owning side to null (unless already changed)
+            if ($message->getSujet() === $this) {
+                $message->setSujet(null);
+            }
+        }
+
+        return $this;
+    }
+}
